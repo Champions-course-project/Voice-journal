76597cdec691abd6f6012cbcb96c9271622fee3a
Lamoshin <127085816+Lamoshin@users.noreply.github.com>
2023-03-23 22:17:20 +1000

    Update check_name.py
    Reworked a bit
    
    Signed-off-by: Lamoshin <127085816+Lamoshin@users.noreply.github.com>

--- SR/check_name.py
+++ SR/check_name.py
@@ -1,6 +1,7 @@
-def check(student_name, word):
-    student_name = student_name.lower().replace('ё', 'е').replace(' ', '').replace('-', '')
-    word = word.lower().replace('ё', 'е').replace(' ', '')
+def check(student_name: str, word: str):
+    student_name = student_name.lower().replace(
+        'ё', 'е').replace(' ', '').replace('-', '')
+    word = word.lower().replace('ё', 'е').replace('-', '')
 
     def full_check(student_name, word):
         if student_name == word:
@@ -8,9 +9,15 @@ def check(student_name, word):
         return False
 
     def part_check(student_name, word):
-        if student_name[0:3] == word[0:3]:
-            return True
-        return False
+        # added so some short names cannot throw an exception because of index being out of range
+        try:
+            if student_name[0:3] == word[0:3]:
+                return True
+            return False
+        except:
+            return False
+
+    # should create much more checks than now!!!
 
     if full_check(student_name, word):
         return 2  # Полное совпадение
